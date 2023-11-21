77aff620f7168402ff9ac7c2193086611b30c695
Joksa132 <nikolajoksimovic419@gmail.com>
2023-11-21 21:08:40 +0100

    create add link functionality

--- app/profile/links/page.tsx
+++ app/profile/links/page.tsx
@@ -1,18 +1,49 @@
+"use client";
+
+import LinkCard from "@/components/LinkCard";
+import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 
 export default function ProfileLinks() {
+  const [linksNumber, setLinksNumber] = useState<number[]>([1]);
+
   return (
     <main className="grid grid-cols-links gap-5 py-5">
-      <section className="bg-white rounded-lg h-full">abc</section>
-      <section className="bg-white rounded-lg h-full px-8 py-10 flex flex-col gap-2">
-        <h2 className="text-3xl font-bold">Customize your links</h2>
-        <span className="text-sm font-medium opacity-50">
-          Add/edit/remove links below and then share all your profiles with the
-          world!
-        </span>
-        <button className="flex justify-center items-center gap-1 py-2 px-6 border-[2px] border-violet-500 rounded-lg text-violet-500 font-semibold text-sm mt-6">
-          <FaPlus size={10} /> Add new link
-        </button>
+      <section className="bg-white rounded-lg h-full"></section>
+      <section className="bg-white rounded-lg flex flex-col h-full">
+        <div className="h-full px-8 py-10 flex flex-col gap-2">
+          <h2 className="text-3xl font-bold">Customize your links</h2>
+          <span className="text-sm font-medium opacity-50">
+            Add/edit/remove links below and then share all your profiles with
+            the world!
+          </span>
+          <button
+            className="flex justify-center items-center gap-1 py-3 px-6 border-[2px] border-violet-500 rounded-lg text-violet-500 font-bold text-lg mt-6 mb-4"
+            onClick={() =>
+              setLinksNumber((prevNumber) => [
+                ...prevNumber,
+                prevNumber.length + 1,
+              ])
+            }
+          >
+            <FaPlus size={10} /> Add new link
+          </button>
+          <div
+            className={`flex flex-col gap-6 max-h-[440px] ${
+              linksNumber.length > 2 ? "overflow-y-scroll" : ""
+            }`}
+          >
+            {linksNumber.map((linkNumber) => (
+              <LinkCard linkNumber={linkNumber} key={linkNumber} />
+            ))}
+          </div>
+        </div>
+        <div className="h-[1.5px] w-full bg-gray-300 bg-opacity-60"></div>
+        <div className="flex justify-end px-8 py-6">
+          <button className="bg-[#633BFE] text-zinc-200 py-3 px-7 rounded-lg font-semibold">
+            Save
+          </button>
+        </div>
       </section>
     </main>
   );
