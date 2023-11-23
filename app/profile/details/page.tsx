9138daba8d86cf44a34fa3b698c48b5e8da91538
Joksa132 <nikolajoksimovic419@gmail.com>
2023-11-23 19:30:53 +0100

    add profile details form

--- app/profile/details/page.tsx
+++ app/profile/details/page.tsx
@@ -7,6 +7,52 @@ export default function ProfileDetails() {
         <span className="text-sm font-medium opacity-50">
           Add your details to create a personal touch to your profile.
         </span>
+        <form className="flex flex-col gap-4 bg-[#FAFAFA] px-4 py-6 rounded-lg">
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="first-name"
+              className="text-sm font-medium opacity-50 w-28 mr-20"
+            >
+              First name*
+            </label>
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              required
+              className="p-2 border border-gray-400 rounded-lg outline-none border-opacity-60 w-full"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="last-name"
+              className="text-sm font-medium opacity-50 w-28 mr-20"
+            >
+              Last name*
+            </label>
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              required
+              className="p-2 border border-gray-400 rounded-lg outline-none border-opacity-60 w-full"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium opacity-50 w-28 mr-20"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className="p-2 border border-gray-400 rounded-lg outline-none border-opacity-60 w-full"
+            />
+          </div>
+        </form>
       </section>
     </main>
   );
