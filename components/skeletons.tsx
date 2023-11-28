f04903e676bed0b131c88f18bd6501dc06062704
Joksa132 <nikolajoksimovic419@gmail.com>
2023-11-28 21:12:01 +0100

    create preview skeletons

---
+++ components/skeletons.tsx
@@ -0,0 +1,15 @@
+export function AvatarSkeleton() {
+  return <div className="h-20 w-20 rounded-full bg-gray-100 shadow-sm"></div>;
+}
+
+export function NameSkeleton() {
+  return <div className="h-3 w-32 rounded-lg bg-gray-100 shadow-sm"></div>;
+}
+
+export function EmailSkeleton() {
+  return <div className="h-2 w-20 rounded-lg bg-gray-100 shadow-sm mb-4"></div>;
+}
+
+export function LinkSkeleton() {
+  return <div className="h-8 w-44 rounded-lg bg-gray-100 shadow-sm"></div>;
+}
