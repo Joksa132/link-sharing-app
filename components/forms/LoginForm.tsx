8f5c03e8ecb07e0371b8fdf5d1018aa0ee1c466c
Joksa132 <nikolajoksimovic419@gmail.com>
2023-12-09 19:19:47 +0100

    create register and login form components

---
+++ components/forms/LoginForm.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { MdOutlineEmail } from "react-icons/md";
+import { IoMdLock } from "react-icons/io";
+import Link from "next/link";
+import { FormEvent } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function LoginForm() {
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const response = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (!response?.error) {
+      router.push("/profile/links");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white rounded-lg px-8 py-10 flex flex-col gap-4"
+    >
+      <div className="flex flex-col gap-2">
+        <h2 className="text-3xl font-bold">Login</h2>
+        <span className="text-sm font-medium opacity-50">
+          Add your details below to get back into the app
+        </span>
+      </div>
+      <div className="flex flex-col mt-6">
+        <label htmlFor="email" className="text-sm font-medium opacity-50 mb-2">
+          Email address
+        </label>
+        <div className="relative">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            className="p-2 border border-gray-400 rounded-lg outline-none border-opacity-60 w-full px-10"
+            placeholder="e.g. alex@email.com"
+          />
+          <MdOutlineEmail className="absolute pointer-events-none bottom-[12px] left-4 text-gray-400" />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <label
+          htmlFor="password"
+          className="text-sm font-medium opacity-50 mb-2"
+        >
+          Password
+        </label>
+        <div className="relative">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            className="p-2 border border-gray-400 rounded-lg outline-none border-opacity-60 w-full px-10"
+            placeholder="Enter your password"
+          />
+          <IoMdLock className="absolute pointer-events-none bottom-[12px] left-4 text-gray-400" />
+        </div>
+      </div>
+      <button className="bg-[#633CFF] text-zinc-200 py-3 rounded-lg font-semibold mt-1">
+        Log in
+      </button>
+      <span className="text-center text-sm mt-2">
+        {"Don't have an account?"}
+        <Link href="/register" className="text-sky-500">
+          Create account
+        </Link>
+      </span>
+    </form>
+  );
+}
