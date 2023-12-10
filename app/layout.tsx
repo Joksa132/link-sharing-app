647434232363d3fae91f13db4eec7b47b8afeedd
Joksa132 <nikolajoksimovic419@gmail.com>
2023-12-10 16:34:30 +0100

    wrap with session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ProfileProvider } from "@/components/context/ProfileContext";
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/components/context/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,15 +12,19 @@ export const metadata: Metadata = {
   description: "Link-Sharing App created in Next.js 14",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
+
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#FAFAFA]`}>
-        <ProfileProvider>{children}</ProfileProvider>
+        <SessionProvider session={session}>
+          <ProfileProvider>{children}</ProfileProvider>
+        </SessionProvider>
       </body>
     </html>
   );
